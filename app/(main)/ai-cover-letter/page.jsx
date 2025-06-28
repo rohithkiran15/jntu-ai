ae891a8704362aaf4896bcaeffbed6c2c972a5cf
rohithkiran15 <rohithkiran6628@gmail.com>
2025-06-28 21:29:13 +0530

    Fixed: Layout of the CL page

--- app/(main)/ai-cover-letter/page.jsx
+++ app/(main)/ai-cover-letter/page.jsx
@@ -1,6 +1,6 @@
 import { getCoverLetters } from "@/action/cover-letter";
 import Link from "next/link";
-import { Plus } from "lucide-react";
+import { ArrowLeft, Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import CoverLetterList from "./_components/cover-letter-list";
 
@@ -9,17 +9,33 @@ export default async function CoverLetterPage() {
 
   return (
     <div>
-      <div className="flex flex-col md:flex-row gap-2 items-center justify-between mb-5">
-        <h1 className="text-6xl font-bold gradient-title">My Cover Letters</h1>
-        <Link href="/ai-cover-letter/new">
-          <Button>
-            <Plus className="h-4 w-4 mr-2" />
-            Create New
-          </Button>
-        </Link>
-      </div>
+      <div className="container mx-auto space-y-4 py-6">
+        <div className="flex flex-col md:flex-row gap-2 items-center justify-between mb-5">
+          <Link href={"/dashboard"}>
+            <Button variant={"link"} className={"gap-2 pl-0"}>
+              <ArrowLeft className="h-4 w-4" />
+              Back to Industry Insights
+            </Button>
+          </Link>
+        </div>
 
-      <CoverLetterList coverLetters={coverLetters} />
+        {/* NEW: Heading and button aligned in one row */}
+        <div className="flex items-center justify-between">
+          <h1 className="text-6xl font-bold gradient-title">
+            My Cover Letters
+          </h1>
+          <Link href="/ai-cover-letter/new">
+            <Button>
+              <Plus className="h-4 w-4 mr-2" />
+              Create New
+            </Button>
+          </Link>
+        </div>
+      </div>
+      <div className="container mx-auto px-4">
+        {/* CoverLetterList renders here */}
+        <CoverLetterList coverLetters={coverLetters} />
+      </div>
     </div>
   );
 }
