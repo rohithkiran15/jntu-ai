aa14a2a44af1237820d12064742857bf3b24d741
rohithkiran15 <rohithkiran6628@gmail.com>
2025-06-28 13:33:39 +0530

    Fix: updated default Theme

--- app/layout.js
+++ app/layout.js
@@ -35,7 +35,7 @@ export default function RootLayout({ children }) {
         >
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
